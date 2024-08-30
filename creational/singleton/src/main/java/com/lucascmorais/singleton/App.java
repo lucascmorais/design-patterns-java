5dcdf6aa7184bcb5a64309bc26554d77224ad8b7
Lucas Costa Morais <lucas.cmorais@hotmail.com>
2024-08-30 00:12:50 -0300

    Fist Commit - Iniciando estudo de Padrões de projeto em Java - 1-Singleton

---
+++ creational/singleton/src/main/java/com/lucascmorais/singleton/App.java
@@ -0,0 +1,11 @@
+package com.lucascmorais.singleton;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}

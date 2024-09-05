cd8a8b9213ca861dccafc2811aa1202f46f79c8a
Lucas Costa Morais <lucas.cmorais@hotmail.com>
2024-09-05 00:01:05 -0300

    estruturando projeto e documentando para iniciar parte pratica - builder

---
+++ creational/builder/src/main/java/com/lucascmorais/builder/App.java
@@ -0,0 +1,11 @@
+package com.lucascmorais.builder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        LOGGER.info("Padrão de projeto Builder");
+    }
+}
